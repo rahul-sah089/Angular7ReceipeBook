83b34abd3b06b1710b326e93a2fc79b29a1c012a
rahul.sah089 <rahul.sah089@gmail.com>
2018-12-30 05:51:07 +0530

    Creating the basic component htmls

--- src/app/receipes/receipe-list/receipe-list.component.ts
+++ src/app/receipes/receipe-list/receipe-list.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Receipe } from '../receipe.model';
 @Component({
   selector: 'app-receipe-list',
   templateUrl: './receipe-list.component.html',
   styleUrls: ['./receipe-list.component.css']
 })
 export class ReceipeListComponent implements OnInit {
-
+  receipes: Receipe[] = [
+  new Receipe("Omelete", "Morning Breakfast", "https://images.media-allrecipes.com/images/55319.jpg?width=420&height=237"),
+  new Receipe("Omelete", "Morning Breakfast", "https://images.media-allrecipes.com/images/55319.jpg?width=420&height=237"),
+  new Receipe("Omelete", "Morning Breakfast", "https://images.media-allrecipes.com/images/55319.jpg?width=420&height=237"),
+  new Receipe("Omelete", "Morning Breakfast", "https://images.media-allrecipes.com/images/55319.jpg?width=420&height=237"),
+  new Receipe("Omelete", "Morning Breakfast", "https://images.media-allrecipes.com/images/55319.jpg?width=420&height=237")
+];
   constructor() { }
 
   ngOnInit() {
